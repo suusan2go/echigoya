6fe00918500b504b7b1ff385a55fafab49cbb184
suzan2go <ksuzuki180@gmail.com>
2016-12-30 23:07:44 +0900

    react helmet setup

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
-const App = () => (
-  <div>
-    Hello React HMR !!!!!!!!
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet title="Home page" />
+        Hello React HMR !!!!!!!!
+      </div>
+    );
+  }
+}
 
 export default App;
